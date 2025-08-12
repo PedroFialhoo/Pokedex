64108abce40d63f6c6af4da24926f302faf0076e
Pedro <pedro.fialho000@gmail.com>
2025-08-12 17:38:31 -0300

    feat: create details.js

--- assets/js/details.js
+++ assets/js/details.js
@@ -9,18 +9,57 @@ function loadPokemonItensByName(name) {
     }
     
 
-    pokeApi.getPokemonsByName(name).then((pokemon = []) => {
+    pokeApi.getPokemonsByName(name).then((pokemon)  => {
             const newHtml = `
-                <div class="pokemon ${pokemon.type}" data-name="${pokemon.name}">
-                    <span class="number">#${pokemon.number}</span>
-                    <span class="name">${pokemon.name}</span>
-                    <div class="detail">
-                        <ol class="types">
-                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                        </ol>
-                        <img src="${pokemon.photo}" alt="${pokemon.name}">
+                    <img src="${pokemon.photo}" alt="" id="poke-img">
+                    <h1 id="pokeName">${pokemon.name}</h1>
+                    <h3>Atributos</h3>
+                    <ol id="atributos">
+                    <li>${pokemon.height} cm</li>
+                    <li>${pokemon.weight} kg</li>
+                    </ol>
+                    <h3>Habilidades</h3>
+                    <ol id="habilidades">
+                        <li>${pokemon.abilities[0]}</li>
+                        <li>${pokemon.abilities[1]}</li>
+                    </ol>
+                    <div id="status">
+                    <div class="container-progress">
+                    <p id="hp"></p>
+                    <div class="progress-bar-container">
+                        <div class="progress-bar" style="width: ${pokemon.status[0]}%;">
+                        </div>
                     </div>
-                </div>
+                    </div>
+                    <div class="container-progress">
+                        <p id="attack"></p>
+                    <div class="progress-bar-container">
+                        <div class="progress-bar" style="width: ${pokemon.status[1]}%;">
+                        </div>
+                    </div>
+                    </div>
+                    <div class="container-progress">
+                        <p id="defense"></p>
+                    <div class="progress-bar-container">
+                        <div class="progress-bar" style="width: ${pokemon.status[2]}%;">
+                        </div>
+                    </div>
+                    </div>
+                    <div class="container-progress">
+                        <p id="special-attack"></p>
+                    <div class="progress-bar-container">
+                        <div class="progress-bar" style="width: ${pokemon.status[3]}%;">
+                        </div>
+                    </div>
+                    </div>
+                    <div class="container-progress">
+                        <p id="speed"></p>
+                    <div class="progress-bar-container">
+                        <div class="progress-bar" style="width: ${pokemon.status[4]}%">
+                        </div>
+                    </div>
+                    </div>
+                    </div>
             `;
 
         pokemonDetail.innerHTML = newHtml
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -12,6 +12,18 @@ function convertPokeApiDetailToPokemon(pokeDetail){
     pokemon.type = type
     pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default
 
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
+
+
+    pokemon.abilities = pokeDetail.abilities.map(
+        (abilitySlot) => abilitySlot.ability.name
+    )
+
+    pokemon.status = pokeDetail.stats.map(
+        (statSlot) => statSlot.base_stat
+    )
+
     return pokemon
 }
 
