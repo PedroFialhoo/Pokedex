161d505210f782c55069936a0428b99251b822bd
Pedro <pedro.fialho000@gmail.com>
2025-08-11 22:58:34 -0300

    feat: pesquisa por nome

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,12 +23,12 @@ function loadPokemonItens(offset, limit){
 
     pokemonList.innerHTML += newHtml
 
-    document.querySelectorAll('#pokemonList li').forEach(li => {
-        li.addEventListener('click', () => {
-            const name = li.getAttribute('data-name');
-            window.location.href = `details.html?pokemon=${name}`;
-        });
-        });
+    // document.querySelectorAll('#pokemonList li').forEach(li => {
+    //     li.addEventListener('click', () => {
+    //         const name = li.getAttribute('data-name');
+    //         window.location.href = `details.html?pokemon=${name}`;
+    //     });
+    //     });
 
     })
 }
